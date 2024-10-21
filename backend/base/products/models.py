bebb9d5ea69f5379144d47470e5ed0f0726df446
Aleks Seriakov <110390382+Fialex1212@users.noreply.github.com>
2024-10-21 03:10:03 +0300

    updated product model

--- backend/base/products/models.py
+++ backend/base/products/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 import uuid
 
-## TODO add addition images, in stock, type like t-shirts, ans subtype,
-## TODO delete new price
-
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
@@ -26,18 +23,29 @@ class Color(models.Model):
         return f"{self.name}"
 
 class Product(models.Model):
+    STATUS_CHOICES = [
+        ('in_stock', 'In Stock'),
+        ('on_the_road', 'On the Road'),
+        ('not_available', 'Not Available'),
+    ]
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=124)
     image = models.ImageField(upload_to="uploads/products/", max_length=255, blank=True, null=True)
     price = models.IntegerField()
     discount = models.IntegerField()
-    new_price = models.IntegerField()
     description = models.TextField()
     colors = models.ManyToManyField(Color)
     size = models.ManyToManyField(Size)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    type = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    availability_status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='in_stock',
+    )
     
     def __str__(self) -> str:
         return f"{self.title}"
